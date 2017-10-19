5d65193d145f646449f4e39be4385d5cfd5624a8
Kenya Tamura <k.tamura1993@gmail.com>
2017-10-19 21:22:09 +0900

    modify

---
+++ Cost.h
@@ -0,0 +1,12 @@
+#ifndef COST_H
+#define COST_H
+
+namespace Cost {
+	int Begin = -10;
+	int Extend = -3;
+	int Match = 1;
+	int Miss = -3;
+}
+
+
+#endif
--- SW.cpp
+++ SW.cpp
@@ -1,6 +1,7 @@
 #include"SW.h"
 #include"Data.h"
 #include"Writer.h"
+#include"Cost.h"
 
 #include<algorithm>
 #include<iostream>
@@ -23,28 +24,38 @@ void SW::DP(const Data& db, const Data& query) {
 	int* FScore = new int[psize] {0};
 	int* MainScore = new int[psize + 1] {0};
 	int EScore = 0;
-	int prevScore = 0;
 	int score = 0;
 	int FBeg = 0;
 	int FExt = 0;
 	int EBeg = 0;
 	int EExt = 0;
+	int match = Cost::Match;
+	int miss = Cost::Miss;
+	int ext = Cost::Extend;
+	int beg = Cost::Begin;
 	for (int t = 0; t < tsize; ++t) {
+		int EScore = 0;
+		int score = 0;
+		int prevScore = 0;
+		int FBeg = 0;
+		int FExt = 0;
+		int EBeg = 0;
+		int EExt = 0;
 		char db_char = db[t];
 		for (int p = 0; p < psize; ++p) {
 			// Match or miss match
 			if (db_char == query[p]) {
-				score = MainScore[p] + 1;
+				score = MainScore[p] + match;
 			}
 			else {
-				score = MainScore[p] - 3;
+				score = MainScore[p] + miss;
 			}
 			// Gap begin
-			FBeg = MainScore[p + 1] - 3;
-			EBeg = prevScore - 10;
+			FBeg = MainScore[p + 1] + beg;
+			EBeg = prevScore + beg;
 			// Gap extend
-			FExt = FScore[p] - 3;
-			EExt = EScore - 3;
+			FExt = FScore[p] + ext;
+			EExt = EScore + ext;
 			// set value
 			MainScore[p] = prevScore;
 			FScore[p] = std::max({ FBeg, FExt, 0 });
