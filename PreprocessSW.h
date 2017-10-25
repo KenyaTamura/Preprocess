bf3cd8085a956cf5dfa8ab04345558f5bc424490
Kenya Tamura <k.tamura1993@gmail.com>
2017-10-25 15:52:32 +0900

    base

---
+++ PreprocessSW.h
@@ -0,0 +1,18 @@
+#ifndef PREPROCESSSW_H
+#define PREPROCESSSW_H
+
+class Preprocess;
+class PreprocessQuad;
+class Data;
+
+class PreprocessSW {
+public:
+	PreprocessSW(const Data& db, const Data& query, const Preprocess& pre, int threshold);
+	~PreprocessSW();
+private:
+	void DP(const Data& db, const Data& query, );
+};
+
+
+
+#endif
